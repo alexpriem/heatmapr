ce65dca9ff5a3115b8475d51e533d3db6ed8517e
alex priem <alexpriem@gmail.com>
2013-11-05 09:04:32 +0100

    importeren testcases bijgewerkt.

--- tests/in.py
+++ tests/in.py
@@ -4,41 +4,27 @@ from localyaml import OrderedDictYAMLLoader
 from heatmapr.db import dbconnection
 
 
-if len(sys.argv)!=2:
-    print 'usage: python in.py <filename>'
+if len(sys.argv)<2:
+    print 'usage: python in.py <filename> [raw|num]'
     print 'imports csv file into db'
     print 'from meta in filename'
+    print 'raw: imports to raw data'
+    print 'num: imports to aggegrate data'
     sys.exit()
 
 
 fname=sys.argv[1]
+dumptype='raw'
+if len(sys.argv)==3:
+    dumptype=sys.argv[2]
+    if dumptype not in('raw','num'):
+        print "dumptype (arg 2) should be 'raw' or 'num'"
+        sys.exit()
 
+        
 f=open(fname)
 desc=f.read()
 f.close()
-
-meta=yaml.load('{'+desc+'}', OrderedDictYAMLLoader)
-metadict={}
-for k,v in meta.items():
-    metadict[k]=v
-
-
-s="""
-drop table if exists {{table}};
-create table {{table}}(
-    {{xcol}} {{xtype}},
-    {{ycol}} {{ytype}}
-    {% if selcol %}
-    , {{selcol}} {{seltype}}
-    {% endif %}
-    );
-""" 
-  
-
-sql=Environment().from_string(s).render(metadict)
-
-d=dbconnection(meta)
-d.run_sql (sql)
 
 
 
@@ -52,6 +38,12 @@ def print_type (val, type_val):
     return str(val)
 
 
+
+meta=yaml.load('{'+desc+'}', OrderedDictYAMLLoader)
+metadict={}
+for k,v in meta.items():
+    metadict[k]=v
+d=dbconnection(meta)
 
 csv=meta['csv']
 delimiter=meta['delimiter']
@@ -72,30 +64,44 @@ xtype=meta['xtype']
 ytype=meta['xtype']
 seltype=meta['seltype']
 
-print len(lines[0].strip().split(','))
+#print len(lines[0].strip().split(','))
 ypixels=len(lines)
 xpixels=len(lines[1].split(','))-1
 ystep=(ymax-ymin)/(ypixels*1.0)
 xstep=(xmax-xmin)/(xpixels*1.0)
 
-print xmin,xmax,xstep
+#print xmin,xmax,xstep
+
+header=meta['xcol']+','+meta['ycol']
+if meta.has_key('selcol'):
+    header+=','+meta['selcol']
+if dumptype=='num':
+    header+=',num'
+header+='\n'
+    
+g.write (header)
 y=ymin
 for l in lines:
     cols=l.strip().split(',')
     x=xmin
-    print len(cols)
+   # print len(cols)
     
-    for c in cols:
-        
-        if c!='':
+    for col in cols:        
+        if col!='':
             s=''
             s+=print_type(x,xtype)
             s+=','
             s+=print_type(y,ytype)
             s+=','
-            s+=print_type(selval,seltype)
-            s+='\n'          
-            g.write(s)
+            s+=print_type(selval,seltype)            
+            if dumptype=='raw':     # 'num' uitvouwen naar num records
+                s+='\n'
+                numval=int(col)
+                for i in range(numval):
+                    g.write(s)
+            if dumptype=='num':
+                s+=','+col+'\n'
+                g.write(s)
             x+=xstep
     y+=ystep
 
