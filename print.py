353291cc0c72e6b4f1884ae428a3d92bf3dbd1db
alex <alexpriem@gmail.com>
2014-07-16 12:00:03 +0200

    printing fixes (works now)

--- print.py
+++ print.py
@@ -1,8 +1,7 @@
 # taken from
 # http://stackoverflow.com/questions/11060439/filling-out-a-form-using-pyqt-and-qwebview
 
-
-import sys
+import os,sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
@@ -15,29 +14,37 @@ web.load(QUrl("./bitmap.html"))
 
 
 def print_pdf():
-    
+    print 'print_pdf', filename
     printer = QPrinter(mode = QPrinter.ScreenResolution)
     printer.setPaperSize(QSizeF(1024, 768), QPrinter.DevicePixel)
     printer.setOutputFormat(QPrinter.PdfFormat)
     printer.setPageMargins(0 , 0 , 0 , 0 , QPrinter.DevicePixel)
     printer.setFullPage(True)
-    printer.setOutputFileName('out.pdf')    
+    printer.setOutputFileName(filename)    
     web.print_(printer)
     print_png()
+    sys.exit(0)
 
 def print_png():
+    print 'print_png', filename
     xpage = web.page()
     xpage.setViewportSize(xpage. currentFrame().contentsSize())
     image = QImage(xpage.viewportSize(),QImage.Format_ARGB32)
     painter = QPainter(image)
     xpage.mainFrame().render(painter)
     painter.end()
-    image.save("out3.png")    
+    image.save(filename)    
     sys.exit(0) 
 
 #web.show()
-QObject.connect(web, SIGNAL("loadFinished(bool)"), print_png)
-#QtCore.QObject.connect(web, QtCore.SIGNAL("loadFinished"), print_it)
-
-
+filename='out.png'
+if len(sys.argv)==2:
+    filename=sys.argv[1]
+print filename, filename[-4:]
+if filename[-4:]=='.png':
+    print 'start_print_png'
+    QObject.connect(web, SIGNAL("loadFinished(bool)"), print_png)
+if filename[-4:]=='.pdf':
+    QObject.connect(web, SIGNAL("loadFinished(bool)"), print_pdf)
+    
 sys.exit(app.exec_())
