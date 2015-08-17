606386671afa30acd9214c62593f711002176524
alex priem <alexpriem@gmail.com>
2015-08-17 22:32:49 +0200

    makeing autoranging also change imgwidth/imgheight to prevent aliasing

--- plus/webmap/view_makemap.py
+++ plus/webmap/view_makemap.py
@@ -36,7 +36,6 @@ def get_colnames_for_heatmap (infodir, heatmaptype, col_info):
 @csrf_exempt
 def make_heatmap (request, dataset, x_var=None, y_var=None):
 
-
     datadir='e:/data'
     infodir=datadir+'/'+dataset+'_info'
     if not os.path.exists(infodir):
@@ -44,49 +43,16 @@ def make_heatmap (request, dataset, x_var=None, y_var=None):
     if not os.path.exists(infodir+'/heatmaps'):
         os.makedirs(infodir+'/heatmaps')
 
-    #print request.path
-    #print request #.META
-    defaults=dict(          
-        sep=',',
-        x_min='perc01',
-        x_max='max',
-        x_steps=500,
-
-        y_min='perc01',
-        y_max='max',
-        y_steps=500,
-
-        split1_var='AR19',
-        split2_var='',
-        gradmin=0,
-        gradmax='max',
-        gradsteps=20,
-        colormap=colormapnames[0],
-
-        displaymode='heatmap',
-        imgwidth=500,
-        imgheight=500
-
-            )
-
     col_info, coltypes_bycol=get_col_types(infodir)
    # print col_info
-    colnames, groupcolnames=get_colnames_for_heatmap (infodir,  defaults['displaymode'], col_info)
+    # args['displaymode'] uit post peuteren.
+    colnames, groupcolnames=get_colnames_for_heatmap (infodir,  'heatmap', col_info)
     if len(colnames)<2:
         msg='Geen heatmaps te maken van deze dataset'
         template = loader.get_template('makemap.html')
         context = RequestContext(request, {'msg':'msg','colnames':colnames, 'dataset':dataset})
         return HttpResponse(template.render(context))
 
-    if x_var is None:
-        defaults['x_var']=colnames[0]
-    else:
-        defaults['x_var']=str(x_var)
-
-    if y_var is None:
-        defaults['y_var']=colnames[1]
-    else:
-        defaults['y_var']=str(y_var)
 
 
     if request.is_ajax()==True:
@@ -105,6 +71,7 @@ def make_heatmap (request, dataset, x_var=None, y_var=None):
             except:                
                 args[key]=val
 
+        print 'x x y:',args['imgheight'], args['imgwidth']
         if cmd=='update':
             col_info, coltypes_bycol=get_col_types(infodir)
             colnames,groupcolnames=get_colnames_for_heatmap (infodir, args['displaymode'], col_info)
@@ -129,38 +96,107 @@ def make_heatmap (request, dataset, x_var=None, y_var=None):
             data={'msg':'ok'}
         return HttpResponse(cjson.encode(data))
 
-    args=defaults
+    args=dict(
+            sep=',',
+            x_min='perc01',
+            x_max='max',
+            x_steps=500,
 
-    xkeys=args['x_steps']
-    setkeys=False
+            y_min='perc01',
+            y_max='max',
+            y_steps=500,
+
+            split1_var='AR19',
+            split2_var='',
+            gradmin=0,
+            gradmax='max',
+            gradsteps=20,
+            colormap=colormapnames[0],
+
+            displaymode='heatmap',
+            imgwidth=500,
+            imgheight=500
+            )
+
+    if x_var is None:
+        args['x_var']=colnames[0]
+    else:
+        args['x_var']=str(x_var)
+
+    if y_var is None:
+        args['y_var']=colnames[1]
+    else:
+        args['y_var']=str(y_var)
+
+
+
     x_types=coltypes_bycol[args['x_var']]   # info van variabelenaam x-kolom ophalen
     y_types=coltypes_bycol[args['y_var']]
-    if x_types['num_keys']<args['x_steps']:
-        xkeys=x_types['num_keys']
-        setkeys=True
-    ykeys=args['y_steps']
-    if y_types['num_keys']<args['y_steps']:
-        ykeys=y_types['num_keys']
-        setkeys=True
-    if setkeys:
-        keys=xkeys
-        if ykeys<xkeys:
-            keys=ykeys
 
-        width=args['imgwidth']
-        stepsizes=[]
-        for i in range(50,0,-1):
-            if (width/(i*1.0))==int(width/i):
-                stepsizes.append(int(width/i))
+    args['x_min']=x_types.get(args['x_min'],args['x_min'])
+    args['y_min']=y_types.get(args['y_min'],args['y_min'])
+    args['x_max']=x_types.get(args['x_max'],args['x_max'])
+    args['y_max']=y_types.get(args['y_max'],args['y_max'])
 
-        prevk=stepsizes[0]
-        for k in stepsizes[1:]:
-            if keys<k:
-                keys=prevk
-                break
-            prevk=k
-        args['x_steps']=keys
-        args['y_steps']=keys
+
+    x_min=args['x_min']
+    x_max=args['x_max']
+    y_min=args['y_min']
+    y_max=args['y_max']
+    dy=y_max-y_min
+    steps_set=False
+    if dy>20 and dy<150:
+        steps_set=True
+        args['y_steps']=dy
+        args['x_steps']=dy
+        if dy<70:
+            args['imgheight']=8*dy
+            args['imgwidth']=8*dy
+        else:
+            args['imgheight']=4*dy
+            args['imgwidth']=4*dy
+    if (steps_set==False) and (dx>20 and dx<150):
+        steps_set=True
+        args['y_steps']=dx
+        args['x_steps']=dx
+        if dx<70:
+            args['imgheight']=8*dx
+            args['imgwidth']=8*dx
+        else:
+            args['imgheight']=4*dx
+            args['imgwidth']=4*dx
+
+    if steps_set==False:
+        xkeys=args['x_steps']       # autorange steps op aantal keys
+        setkeys=False
+        x_types=coltypes_bycol[args['x_var']]   # info van variabelenaam x-kolom ophalen
+        y_types=coltypes_bycol[args['y_var']]
+        if x_types['num_keys']<args['x_steps']:
+            xkeys=x_types['num_keys']
+            setkeys=True
+        ykeys=args['y_steps']
+        if y_types['num_keys']<args['y_steps']:
+            ykeys=y_types['num_keys']
+            setkeys=True
+        if setkeys:
+            keys=xkeys
+            if ykeys<xkeys:
+                keys=ykeys
+
+            width=args['imgwidth']
+            stepsizes=[]
+            for i in range(50,0,-1):
+                if (width/(i*1.0))==int(width/i):
+                    stepsizes.append(int(width/i))
+
+            prevk=stepsizes[0]
+            for k in stepsizes[1:]:
+                if keys<k:
+                    keys=prevk
+                    break
+                prevk=k
+            args['x_steps']=keys
+            args['y_steps']=keys
 
     labels=read_csvfile (infodir+'/labels.csv')
     col_x=x_types['colname']
@@ -169,6 +205,7 @@ def make_heatmap (request, dataset, x_var=None, y_var=None):
     args['y_label']=labels.get(col_y,col_y)
     args['title']=labels.get(col_x,col_x)+' vs '+labels.get(col_y,col_y)
 
+    print args['y_steps']
     args_json=cjson.encode(args)
     template = loader.get_template('makemap.html')    
     context = RequestContext(request, {'colnames':colnames,
